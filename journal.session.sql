19907fc57a72b0eaa369fd7af7c6c8e63b9cab57
VIKRAM067 <vvikram0678@gmail.com>
2024-07-06 21:21:28 +0530

    Refactor: sql changes

--- journal.session.sql
+++ journal.session.sql
@@ -1,9 +1,10 @@
 CREATE TABLE Journal(
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    date DATE ,
-    topic VARCHAR(200) ,
-    notes TEXT
+    date DATE NOT NULL ,
+    topic VARCHAR(200) NOT NULL ,
+    notes TEXT 
 );
 
 DESCRIBE journal ;
 
+DROP TABLE journal;
